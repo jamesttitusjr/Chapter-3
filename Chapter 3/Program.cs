96436223534ce20ed4a98019d449c74a4b6b4fdd
James Titus <jamesttitusjr@outlook.com>
2022-08-26 14:24:57 -0400

    Add project files.

---
+++ Chapter 3/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+Console.WriteLine("Hello, World!");
+
+//Increment Operator
+int n = 0;
+// Basic Code
+n = n + 1;
+// Incriment Operator used majority of the time.
+n++; // n is 2 first time
+n++; // n is 3 secon time
+n--; // n is 2 for minus
+
+int a = 0;
+int b = a++; // b is 0 and a is 1 ++ on right side 
+int c = ++a; // c is 2 and a is 2 ++ on left side
+
+// int Operator
+int a = 1;
+int b = +a; // b is 1
+int c = -a; // c is -1
+a = -1;
+b = +a; // b is -1
+c = -a; // c is 1
+// - sign is used the most
+
+// pg. 97 Binary = must use 2 of them. =-*/
+
+// Modulo Operator 4?
+
+// Assignment Operator for use in all cases except when using 1 (Incriment).
+int n = 0;
+n = n + 5;
+n += 5;
+
+
